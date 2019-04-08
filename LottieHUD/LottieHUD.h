f0c4e49542f9650175dd8fb943b043dcafc7060c
Ahmed Raad <ahmedraad3333@gmail.com>
2019-04-08 19:49:05 +0300

    Initialize

---
+++ LottieHUD/LottieHUD.h
@@ -0,0 +1,19 @@
+//
+//  LottieHUD.h
+//  LottieHUD
+//
+//  Created by Ahmed Raad on 4/8/19.
+//  Copyright © 2019 Ahmed Raad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LottieHUD.
+FOUNDATION_EXPORT double LottieHUDVersionNumber;
+
+//! Project version string for LottieHUD.
+FOUNDATION_EXPORT const unsigned char LottieHUDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LottieHUD/PublicHeader.h>
+
+
